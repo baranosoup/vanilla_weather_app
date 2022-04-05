0370c3f0ebafc2b1516602cbccf0ce5b424462be
baranosoup <95305948+baranosoup@users.noreply.github.com>
2022-04-05 16:10:17 -0500

    created javascript functions for location search

---
+++ src/app.js
@@ -0,0 +1,31 @@
+function searchCity(city) {
+  let apiKey = "1de6f8762746de7203bc01157efb2611";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  searchCity(cityInputElement.value);
+}
+
+function searchLocation(position) {
+  let apiKey = "1de6f8762746de7203bc01157efb2611";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("London");
+displayForecast();
